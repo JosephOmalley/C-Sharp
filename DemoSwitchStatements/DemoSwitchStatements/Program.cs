3555239c67b95990f1621006dbf5531da0924034
Joe <josephomalley135@gmail.com>
2021-04-29 16:19:15 -0500

    Example of while loop and do while

---
+++ DemoSwitchStatements/DemoSwitchStatements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace DemoSwitchStatements
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("This is an example of a while loop");
+            int i = 0;
+            do // do while allows the program to enter the while loop atleast once even if the condition is met 
+            {
+                Console.WriteLine(i);
+                i++;
+                Thread.Sleep(100);
+            }
+            while (i < 5);
+            Console.Read();
+        }
+    }
+}
