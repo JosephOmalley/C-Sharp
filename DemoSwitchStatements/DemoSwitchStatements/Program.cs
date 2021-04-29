52f857460b57b4bac2820f30592f754be7c57094
Joe <josephomalley135@gmail.com>
2021-04-29 18:33:35 -0500

    add plain while loop

--- DemoSwitchStatements/DemoSwitchStatements/Program.cs
+++ DemoSwitchStatements/DemoSwitchStatements/Program.cs
@@ -10,16 +10,43 @@ namespace DemoSwitchStatements
     {
         static void Main()
         {
-            Console.WriteLine("This is an example of a while loop");
-            int i = 0;
-            do // do while allows the program to enter the while loop atleast once even if the condition is met 
+
+        // do while example
+        Console.WriteLine("This is an example of a do while loop");
+        int i = 0;
+        do // do while allows the program to enter the while loop atleast once even if the condition is met 
+        {
+            Console.WriteLine(i);
+            i++;
+            Thread.Sleep(100);
+        }
+        while (i < 5);
+        
+        //while loop example
+        Console.WriteLine("This is an example of a plain while loop");
+        bool track = true;
+        while (track)
+        {
+            Console.WriteLine("What's is the sum of 4 and 9?");
+            int result = Convert.ToInt32(Console.ReadLine());
+            
+            if (result == 13)
             {
-                Console.WriteLine(i);
-                i++;
-                Thread.Sleep(100);
+                Console.WriteLine("13 is the right answer!");
+                track = false;
             }
-            while (i < 5);
-            Console.Read();
+            else
+            {
+                Console.WriteLine(result + " is not the right answer, please try again.");
+
+            }
+        }
+        Console.WriteLine("Press any key to stop to program");
+        Console.ReadLine();
+        return;
+
+
+
         }
     }
 }
---
+++ arrays_and_lists/arrays_and_lists/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        List<int> intlist = new List<int>();
+        intlist.Add(4);
+        intlist.Add(10);
+        intlist.Remove(10);
+
+        Console.WriteLine(intlist[0]);
+        Console.ReadLine();
+    }
+}
+
