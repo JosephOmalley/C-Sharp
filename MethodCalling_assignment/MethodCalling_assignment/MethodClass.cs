8c4b1ab07b8e97f96268303ceeef5edaf9055fc6
Joe <josephomalley135@gmail.com>
2021-05-04 17:01:57 -0500

    add method calling assignment

---
+++ MethodCalling_assignment/MethodCalling_assignment/MethodClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodCalling_assignment
+{
+    public class MethodClass
+    {
+        public static void AddMethod(int num) //since this method does not contain a return statement, but has writeline which returns a string it has static and void 
+        {
+            Console.WriteLine(num + 1);
+        }
+        public static void FindTip(int bill)
+        {
+            Console.WriteLine(bill * .20);
+        }
+        public static void doubleNum(int num)
+        {
+            Console.WriteLine(num * 2);
+        }
+    }
+}
---
+++ MethodCalling_assignment/MethodCalling_assignment/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodCalling_assignment
+{
+    class Program
+    {
+        static void Main(string[] args) //entrance point of the program
+        {
+            
+            bool check = true;
+            while (check)//Loops until proper input 
+            {
+                try // ensure the user enters valid data type
+                {
+                    Console.WriteLine("enter a number to add it to 1");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    MethodClass.AddMethod(num);
+                    check = false;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Input must be a whole number");
+                }
+            }
+
+
+
+            bool check1 = true;
+            while (check1)//Loops until proper input 
+            {
+                try // ensure the user enters valid data type
+                {
+                    Console.WriteLine("enter a number to double it ");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    MethodClass.doubleNum(num1);
+                    check1 = false;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Input must be a whole number");
+                };
+            }
+
+            
+            bool check2 = true;
+            while (check2)//Loops until proper input 
+            {
+
+                try // ensure the user enters valid data type
+                {
+                    Console.WriteLine("Enter the total of the bill to find tip amount");
+                    int bill = Convert.ToInt32(Console.ReadLine());
+                    MethodClass.FindTip(bill);
+                    Console.ReadLine();
+                    check2 = false;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Input must be a whole number");
+                }
+            }
+            Console.WriteLine("Press any key to exit the program");//Where the program exits
+            Console.ReadLine();
+
+        }
+
+    }
+}
