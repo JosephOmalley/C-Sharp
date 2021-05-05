584ab3a622febfc251b4a0dd441580f00624d7c1
Joe <josephomalley135@gmail.com>
2021-05-04 20:11:05 -0500

    add 3 overloaded methods.

---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            classwithmethod.add2(5);//instantiates the class along with calling the method 
+            classwithmethod.add2(2.00);
+            classwithmethod.add2(Convert.ToInt32("2"));
+            Console.Read();
+        }
+        
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/classwithmethod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class classwithmethod
+    {
+        public static void add2(int num)
+        {
+            int result = num + 2;
+            Console.WriteLine(result);
+
+        }
+        public static void add2(double num)
+        {
+            double result = num + 2.00;
+            Console.WriteLine(result);
+        }
+        public static void add2(string num)
+        {
+            string result = num + 2;
+            Console.WriteLine(result);
+        }
+
+
+
+    }
+}
