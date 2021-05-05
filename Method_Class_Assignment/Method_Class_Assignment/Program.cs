7e0a103410bc9cc93db7243bf9fde05903eafc6f
Joe <josephomalley135@gmail.com>
2021-05-05 17:58:43 -0500

    Method Class assignment

---
+++ Method_Class_Assignment/Method_Class_Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidClass.VoidMethod(num1: 1,num2: 2);//instatiated class then method called, both parameters name 
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Method_Class_Assignment/Method_Class_Assignment/VoidClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class VoidClass
+    {
+        public static void VoidMethod(int num1, int num2)
+        {
+            Console.WriteLine(num1 + 1);
+            Console.WriteLine(num2);
+        }
+    }
+}
