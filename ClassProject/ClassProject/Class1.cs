be41d2d1c7639dcb2f7bc0e8f8444e8f5ed377cf
Joe <josephomalley135@gmail.com>
2021-05-05 19:06:34 -0500

    Add method that cuts numbers in half add method without parameters add overloaded method add static class

---
+++ ClassProject/ClassProject/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProject
+{
+    static class Class1
+    {
+        public static void MethodHalf(int num1)
+        {
+            int result = num1 / 2;
+            Console.WriteLine(result );
+        }
+        public static void MethodHalf(string num1)
+        {
+            int result = Convert.ToInt32(num1) / 2;
+            Console.WriteLine(result);
+        }
+        public static void noParaMethod()
+        {
+            Console.WriteLine("This method takes in no parameters");
+        }
+    }
+}
---
+++ ClassProject/ClassProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int uNum = Convert.ToInt32(Console.ReadLine());
+            Class1.MethodHalf(uNum);
+            Class1.noParaMethod();
+            Console.WriteLine("Press enter key to exit");
+            Console.ReadLine();
+        }
+    }
+}
