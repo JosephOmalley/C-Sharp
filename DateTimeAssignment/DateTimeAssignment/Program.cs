57a98c4e29468792a0506eda1afbfa2ed47c81b7
Joe <josephomalley135@gmail.com>
2021-05-17 00:46:27 -0500

    commit DateTime assignment

---
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Enter a number to see what hour it\'ll be in that amount of time.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(DateTime.Now.AddHours(x));//adds an a user determinded amount of hours
+            Console.ReadLine();
+        }
+    }
+}
---
+++ InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int userInt = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Josep\Desktop\log.txt", true))
+            {
+                file.WriteLine(userInt);
+            }
+            Console.WriteLine("This is the data in log.txt file\n");
+            using (StreamReader rFile = new StreamReader(@"C:\Users\Josep\Desktop\log.txt", true))
+            {
+                Console.WriteLine(rFile.ReadToEnd());
+            }
+            Console.ReadLine();
+        }
+    }
+}
