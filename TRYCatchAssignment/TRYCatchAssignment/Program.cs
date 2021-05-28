2b73e5b464ca61f2b1705d54ab539f6147012d9d
Joe <josephomalley135@gmail.com>
2021-05-27 23:21:07 -0500

    GetQuote and Admin page

---
+++ TRYCatchAssignment/TRYCatchAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRYCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("How old are you?");
+                int uAge = Convert.ToInt32(Console.ReadLine());
+                if (uAge <= 0)
+                {
+                    throw new ArgumentException();
+                }
+                Console.WriteLine("You were born in " + DateTime.Now.AddYears(-uAge + -1).Year);
+                Console.ReadLine();
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invaild input. make sure your entry was not zero, or a negitive number");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Input must be a digit");
+
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
